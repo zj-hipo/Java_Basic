bb4cb731271d3da4cbffdadef7346d09751ec26f
zj-hipo <542699293@qq.com>
2020-06-02 10:10:09 +0800

    测试

---
+++ src/main/java/com/zj/javabasic/map/HasnMapTest.java
@@ -0,0 +1,14 @@
+package com.zj.javabasic.map;
+
+import java.util.HashMap;
+
+/**
+ * @program: javabasic
+ * @description: 填写描述
+ * @author: 张军
+ * @create: 2019-11-29 13:29
+ **/
+
+public class HasnMapTest {
+    private HashMap hashMap;
+}
